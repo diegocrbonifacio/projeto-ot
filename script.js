535fcb1bfd0de91213d76ce2fbc43988a729d2ac
diegocrbonifacio <125907031+diegocrbonifacio@users.noreply.github.com>
2023-05-28 18:47:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+const resultDiv = document.getElementById('result');
+
+let recognition;
+
+startBtn.addEventListener('click', () => {
+  recognition = new webkitSpeechRecognition();
+  recognition.lang = 'pt-BR'; // Define o idioma para reconhecimento (pode ser alterado para outros idiomas)
+  recognition.start();
+  recognition.addEventListener('result', onRecognitionResult);
+});
+
+stopBtn.addEventListener('click', () => {
+  recognition.stop();
+  recognition.removeEventListener('result', onRecognitionResult);
+});
+
+function onRecognitionResult(event) {
+  const transcript = event.results[0][0].transcript;
+  resultDiv.innerHTML += `<p>${transcript}</p>`;
+}
