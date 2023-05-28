7cd3a368743b6a326265d87e0e4e5e57cac41cdd
diegocrbonifacio <125907031+diegocrbonifacio@users.noreply.github.com>
2023-05-28 18:54:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,74 @@
-const startBtn = document.getElementById('start-btn');
-const stopBtn = document.getElementById('stop-btn');
-const resultDiv = document.getElementById('result');
+(function () {
 
-let recognition;
 
-startBtn.addEventListener('click', () => {
-  recognition = new webkitSpeechRecognition();
-  recognition.lang = 'pt-BR'; // Define o idioma para reconhecimento (pode ser alterado para outros idiomas)
-  recognition.start();
-  recognition.addEventListener('result', onRecognitionResult);
-});
+    var speakBtn = document.querySelector('#speakbt');
+    var resultSpeaker = document.querySelector('#resultSpeak');
 
-stopBtn.addEventListener('click', () => {
-  recognition.stop();
-});
+    if (window.SpeechRecognition || window.webkitSpeechRecognition) {
 
-recognition.addEventListener('result', onRecognitionResult);
+        var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 
-function onRecognitionResult(event) {
-  const transcript = event.results[0][0].transcript;
-  resultDiv.innerHTML += `<p>${transcript}</p>`;
-}
+        var myRecognition = new SpeechRecognition();
+
+        myRecognition.lang = 'pt-BR';
+
+
+        speakBtn.addEventListener('click', function () {
+
+            try {
+
+                myRecognition.start();
+
+                resultSpeaker.innerHTML = "Estou te ouvindo!";
+
+            } catch (erro) {
+                alert('erro:' + erro.message);
+            }
+
+        }, false);
+
+        myRecognition.addEventListener('result', function (evt) {
+
+            var resultSpeak = evt.results[0][0].transcript;
+
+            console.log(resultSpeak);
+
+            resultSpeaker.innerHTML = resultSpeak;
+
+            switch (resultSpeak.toLowerCase()) {
+                case 'clarear':
+                    document.body.style.backgroundColor = '#33cc99';
+                    break;
+                case 'escurecer':
+                    document.body.style.backgroundColor = '#047751';
+                    break;
+                case 'meu site':
+                    window.location.href = 'https://www.jetersonlordano.com.br';
+                    break;
+            }
+
+            if (resultSpeak.match(/buscar por/)) {
+
+                resultSpeaker.innerHTML = 'Redirecionando...';
+
+                setTimeout(function () {
+
+                    var resultado = resultSpeak.split('buscar por');
+                    window.location.href = 'https://www.google.com.br/search?q=' + resultado[1];
+
+                }, 2000);
+            }
+
+        }, false);
+
+        myRecognition.addEventListener('error', function (evt) {
+
+            resultSpeaker.innerHTML = 'Se você disse alguma coisa, não ouvi muito bem!';
+
+        }, false);
+
+    } else {
+        resultSpeaker.innerHTML = 'Seu navegador não suporta tanta tecnoligia!';
+    }
+
+})();
